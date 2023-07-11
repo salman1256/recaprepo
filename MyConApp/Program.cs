27d0f5c0633b9f50218aea5f74b00f46a59e8377
salman1256@yahoo.com <salman1256@yahoo.com>
2023-07-11 17:12:54 +0530

    Welcome change

--- MyConApp/Program.cs
+++ MyConApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyConApp
 {
@@ -10,6 +6,8 @@ namespace MyConApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Git Today @: "+DateTime.Now);
+            Console.ReadKey();
         }
     }
 }
